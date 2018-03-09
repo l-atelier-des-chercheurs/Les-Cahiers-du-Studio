8513ffc3185f65d08a1ed4e5270fca1c601dc383
Louis Eveillard <barlou15+github@gmail.com>
2018-03-09 18:52:25 +0100

    fixed key

--- public/src/vue/components/BottomFooter.vue
+++ public/src/vue/components/BottomFooter.vue
@@ -8,7 +8,7 @@
         <template v-for="(ip, index) in $root.state.localNetworkInfos.ip">
           <a
             :href="getURLToApp(ip, $root.state.localNetworkInfos.port)"
-            :key="index"
+            :key="`${index}_${ip}`"
             class="js--openInBrowser"
             target="_blank"
             v-html="getURLToApp(ip, $root.state.localNetworkInfos.port)">
