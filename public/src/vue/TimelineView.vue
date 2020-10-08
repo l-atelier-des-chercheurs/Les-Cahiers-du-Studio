bd3bb9460ee91499107ff1d9c9afb194472b3a9c
Louis Eveillard <barlou15+github@gmail.com>
2020-10-08 17:53:18 +0200

    disabled folding

--- public/src/vue/TimelineView.vue
+++ public/src/vue/TimelineView.vue
@@ -576,11 +576,11 @@ export default {
     this.$root.settings.sidebar_type = "informations";
     this.$root.settings.has_sidebar_opened = true;
 
-    const startDate = this.$moment(1598191200000);
-    while (startDate.diff(this.$moment().subtract(1, "days")) < 0) {
-      this.folded_days.push(+startDate);
-      startDate.add(1, "days").startOf("day");
-    }
+    // const startDate = this.$moment(1598191200000);
+    // while (startDate.diff(this.$moment().subtract(1, "days")) < 0) {
+    //   this.folded_days.push(+startDate);
+    //   startDate.add(1, "days").startOf("day");
+    // }
 
     this.$eventHub.$on("scrollToMedia", this.scrollToMedia);
     this.$eventHub.$on("scrollToDate", this.scrollToDate);
