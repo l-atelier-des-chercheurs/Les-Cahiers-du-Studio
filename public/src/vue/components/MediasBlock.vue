660fffed56a4ecfb3cfa06fb9e2f20f739e93ea2
Louis Eveillard <barlou15+github@gmail.com>
2021-04-15 16:30:32 +0200

    UI / removed grid

--- public/src/vue/components/MediasBlock.vue
+++ public/src/vue/components/MediasBlock.vue
@@ -122,20 +122,20 @@ export default {
   --grid-color-vertical: transparent;
 
   // background-image: repeating-linear-gradient(-90deg,transparent,transparent var(--gridstep_before),var(--grid-color) var(--gridstep_before),var(--grid-color) var(--gridstep)),repeating-linear-gradient(180deg,transparent,transparent var(--gridstep_before),var(--grid-color) var(--gridstep_before),var(--grid-color) var(--gridstep));
-  background-image: repeating-linear-gradient(
-      -90deg,
-      transparent,
-      transparent var(--gridstep_before),
-      var(--grid-color-vertical) var(--gridstep_before),
-      var(--grid-color-vertical) var(--gridstep)
-    ),
-    repeating-linear-gradient(
-      180deg,
-      transparent,
-      transparent var(--gridstep_before),
-      var(--grid-color-horizontal) var(--gridstep_before),
-      var(--grid-color-horizontal) var(--gridstep)
-    );
+  // background-image: repeating-linear-gradient(
+  //     -90deg,
+  //     transparent,
+  //     transparent var(--gridstep_before),
+  //     var(--grid-color-vertical) var(--gridstep_before),
+  //     var(--grid-color-vertical) var(--gridstep)
+  //   ),
+  //   repeating-linear-gradient(
+  //     180deg,
+  //     transparent,
+  //     transparent var(--gridstep_before),
+  //     var(--grid-color-horizontal) var(--gridstep_before),
+  //     var(--grid-color-horizontal) var(--gridstep)
+  //   );
   background-repeat: no-repeat;
   background-size: 100% 100%;
 
